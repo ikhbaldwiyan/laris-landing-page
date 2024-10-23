f870bafc9e7ce36eb7cc3b82596f7a55173d3a64
Ikhbal Dwiyantoro <ikhbal.d@smt-id.com>
2024-10-23 21:57:16 +0700

    sliciing header and main section

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#B7EB38",
+        secondary: '#0B251C',
+        background: "#EDF3F1",
+      },
+    },
+  },
+  plugins: [],
+}
